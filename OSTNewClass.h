2a477f76177aaf655df6f20a13c639e85520bb45
Justin <justin@Justins-MacBook-Pro.local>
2015-07-28 18:14:14 -0700

    added test class

---
+++ OSTNewClass.h
@@ -0,0 +1,13 @@
+//
+//  OSTNewClass.h
+//  Git Test
+//
+//  Created by Justin on 7/28/15.
+//  Copyright (c) 2015 Ocean Sky Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OSTNewClass : NSObject
+
+@end
---
+++ OSTNewClass.m
@@ -0,0 +1,13 @@
+//
+//  OSTNewClass.m
+//  Git Test
+//
+//  Created by Justin on 7/28/15.
+//  Copyright (c) 2015 Ocean Sky Technology. All rights reserved.
+//
+
+#import "OSTNewClass.h"
+
+@implementation OSTNewClass
+
+@end
